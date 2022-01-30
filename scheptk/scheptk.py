a493bd6f1cc7cdb5042d5405eb92d408298cc998
Jose M Framiñán <framinan@us.es>
2022-01-30 20:19:21 +0100

    First structure of folders

--- scheptk/scheptk.py
+++ scheptk/scheptk.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod #abstract classes
-from util import read_tag, print_tag_value, print_tag_vector
+#import ast # to get the proper data type
+import sys # to stop the exectuion (funcion exit() )
+
+
+from scheptk.util import print_tag_value, print_tag_vector, get_proper_type, read_tag
+
 
 class Instance(ABC):
  
@@ -18,35 +23,71 @@ class Instance(ABC):
     def read_basic_data(self, filename):
         # jobs (mandatory data)
         self.jobs = read_tag(filename,"JOBS")
+        # if jobs = -1 the program cannot continue
+        if(self.jobs ==-1):
+            print("No jobs specified. The program cannot continue.")
+            sys.exit()
+        # processing times (mandatory data)
         self.pt = read_tag(filename,"PT")
-        self.w = read_tag(filename, "W")
+        if(self.pt ==-1):
+            print("No processing times specified. The program cannot continue.")
+            sys.exit()
+        else:
+            if(len(self.pt) != self.jobs ):
+                print("Number of processing times does not match the number of jobs (JOBS={}, length of PT={}). The program cannot continue".format(self.jobs, len(self.pt)) )
+                sys.exit()                
         # weights (if not, default weights)
+        self.w = read_tag(filename, "W")
         if(self.w ==-1):
             self.w = [1.0 for i in range(self.jobs)]
-            print("No weights specified for the jobs. All assummed to be 1.0.")    
-
+            print("No weights specified for the jobs. All weights set to 1.0.")    
         # due dates (if not,  -1 is assumed)
         self.dd = read_tag(filename, "DD")
         if(self.dd ==-1):
-            print("No due dates specified for the jobs. All assummed to be infinite.")    
-        
+            print("No due dates specified for the jobs. All due dates assummed to be infinite.")           
         # release dates (if not, 0 is assumed)
         self.r = read_tag(filename,"R")
         if(self.r==-1):
             self.r = [0 for i in range(self.jobs)]
-            print("No release dates specified for the jobs. All assummed to be zero.")    
+            print("No release dates specified for the jobs. All release dates set to zero.")    
 
     # print basic data
     def print_basic_data(self):
         print_tag_value("JOBS", self.jobs)
-        print_tag_vector("W", self.w)
         print_tag_vector("PT", self.pt)
-        #print("[W=")
-        #print(self.w)
+        print_tag_vector("DD", self.dd)
+        print_tag_vector("R", self.r)
+        print_tag_vector("W", self.w)
+
    
     # concrete method makespan
     def Cmax(self, sequence):
         return max(self.ct(sequence))
+
+    # max earliness
+    def Emax(self, sequence):
+        if(self.dd == -1):
+            print("The instance does not have due dates and due-date related objective cannot be computed. The function will return -1")
+            return -1
+        else:
+            earliness = []
+            completion_times = self.ct(sequence)
+            for index, item in enumerate(completion_times):
+                earliness.append(max(self.dd[sequence[index]]-item,0))
+
+        return max(earliness)
+
+    # max lateness
+    def Lmax(self, sequence):
+        if(self.dd == -1):
+            print("The instance does not have due dates and due-date related objective cannot be cmputed.")
+        else:
+            lateness = []
+            completion_times = self.ct(sequence)
+            for index, item in enumerate(completion_times):
+                lateness.append(item-self.dd[sequence[index]])
+        
+        return max(lateness)
 
     # sum of completion tme
     def SumCj(self, sequence):
@@ -65,36 +106,22 @@ class Instance(ABC):
 
         return sum(earliness)
 
-    # max earliness
-    def Emax(self, sequence):
+    # sum lateness
+    def SumLj(self, sequence):
         if(self.dd == -1):
-            print("The instance does not have due dates and due-date related objective cannot be computed. The function will return -1")
-            return -1
+            print("The instance does not have due dates and due-date related objective cannot be computed.")
         else:
-            earliness = []
+            lateness = []
             completion_times = self.ct(sequence)
             for index, item in enumerate(completion_times):
-                earliness.append(max(self.dd[sequence[index]]-item,0))
-
-        return max(earliness)
-
-
-    # sum earliness
-    def SumEj(self, sequence):
-        if(self.dd == -1):
-            print("The instance does not have due dates and due-date related objective cannot be cmputed.")
-        else:
-            tardiness = []
-            completion_times = self.ct(sequence)
-            for index, item in enumerate(completion_times):
-                tardiness.append(max(self.dd[sequence[index]]-item,0))
-
-        return sum(tardiness)
+                lateness.append(item-self.dd[sequence[index]])
+        
+        return sum(lateness)
 
     # sum tardiness
     def SumTj(self, sequence):
         if(self.dd == -1):
-            print("The instance does not have due dates and due-date related objective cannot be cmputed.")
+            print("The instance does not have due dates and due-date related objective cannot be computed.")
         else:
             tardiness = []
             completion_times = self.ct(sequence)
@@ -110,3 +137,29 @@ class Instance(ABC):
         for j in range(len(sequence)):
             wjcj = wjcj + ct[j] * self.w[sequence[j]]
         return wjcj
+
+
+# class to implement the single machine layout
+class SingleMachine(Instance):
+  
+   
+    def __init__(self, filename):
+        print("----- Reading SingleMachine instance data from file " + filename + " -------")
+        self.read_basic_data(filename)
+        self.print_basic_data()
+        print("----- end of SingleMachine instance data from file " + filename + " -------")
+        
+
+
+    def ct(self,sequence):
+        completion_time = []
+        completion_time.append(self.r[sequence[0]] + self.pt[sequence[0]])
+        for i in range(1,len(sequence)):
+            completion_time.append(max(completion_time[i-1],self.r[sequence[i]]) + self.pt[sequence[i]])
+        return completion_time
+
+    
+
+
+
+       
--- scheptk/util.py
+++ scheptk/util.py
@@ -1,5 +1,30 @@
-import ast # to get the proper data type
 
+import ast # to get the proper data type
+import operator # for sorted functions
+
+# utility function to get the proper type (int, string, float) of an argument
+def get_proper_type(x):
+    if isinstance(ast.literal_eval(x), int):
+        return int(x)
+    else:
+        if isinstance(ast.literal_eval(x), float):
+            return float(x)
+        else:
+            return x
+
+
+# utility function to print the content of a value (escalar) with a tag
+def print_tag_value(tag, value):
+    print("[" + tag + "=" + str(value) + "]")     
+
+
+# utility function to print the content of a vector with a tag
+def print_tag_vector(tag, vector):
+    cadena = "[" + tag + "="
+    for i in range(len(vector)-1):
+        cadena = cadena + str(vector[i]) + ","
+    cadena = cadena + str(vector[len(vector)-1]) + "]"
+    print(cadena)    
 
 
 # utility function to read a tag
@@ -12,7 +37,7 @@ def read_tag(filename, tag):
             line_number = line_number+1
             found = lines[line_number].find("["+tag + "=")
         if(found ==-1):
-            print("Tag " + tag + " does not exist in file " + filename)
+            print("Tag " + tag + " does not exist in file " + filename + ". ", end="")
             return -1
         else:
             tag_value = lines[line_number][found + len(tag)+2:len(lines[line_number])-2]
@@ -34,12 +59,29 @@ def read_tag(filename, tag):
                     return values
 
 
-# utility function to get the proper type (int, string, float) of an argument
-def get_proper_type(x):
-    if isinstance(ast.literal_eval(x), int):
-        return int(x)
-    else:
-        if isinstance(ast.literal_eval(x), float):
-            return float(x)
-        else:
-            return x
+# utility function to obtain the index of a sorted list. It is used internally, most of the times use sorted_index_asc or sorted_index_desc
+def sorted_index(list, descending):
+    tuple = enumerate(list)
+    sorted_tuple = sorted(tuple, key=operator.itemgetter(1), reverse=descending)
+    sorted_index = [index for index,item in sorted_tuple]
+    return sorted_index
+
+def sorted_index_asc(list):
+    return sorted_index(list, False)
+
+def sorted_index_desc(list):
+    return sorted_index(list, True)
+
+
+# utility function to obtain the value of a sorted list. It is used internally, most of the times use sorted_value_asc or sorted_value_desc
+def sorted_value(list, descending):
+    tuple = enumerate(list)
+    sorted_tuple = sorted(tuple, key=operator.itemgetter(1), reverse=descending)
+    sorted_list = [item for index,item in sorted_tuple]
+    return sorted_list    
+
+def sorted_value_asc(list):
+    return sorted_value(list, False)
+
+def sorted_value_desc(list):
+    return sorted_value(list, True)
